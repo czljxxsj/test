0e4fcd1ab39f90cfb7ac316faf41d5827b990886
tangchongbo <tangchongbo@agibot.com>
2026-01-18 09:26:42 +0800

    add a loop

--- hello.cpp
+++ hello.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
-
 using namespace std;
 
 int main() {
     cout << "Hello, Tangchongbo! This is my first C++ program." << endl;
+
+    // 新增的循环代码
+    for(int i = 0; i < 3; i++) {
+        cout << "Count: " << i << endl;
+    }
+
     return 0;
 }
